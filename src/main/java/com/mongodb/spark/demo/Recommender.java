ea24e719fa657a57df68747ddabef58b8fa9d6c3
Sandeep Parikh <sandeep@clusterbeep.org>
2014-09-18 13:54:53 -0500

    updates for mvn artifacts and some logging

--- src/main/java/com/mongodb/spark/demo/Recommender.java
+++ src/main/java/com/mongodb/spark/demo/Recommender.java
@@ -7,27 +7,25 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.function.FlatMapFunction;
 import org.apache.spark.api.java.function.Function;
 import org.apache.spark.api.java.function.PairFunction;
 import org.apache.spark.mllib.recommendation.MatrixFactorizationModel;
 import org.apache.spark.mllib.recommendation.Rating;
 import org.apache.spark.mllib.recommendation.ALS;
-import org.apache.spark.storage.StorageLevel;
 import org.bson.BSONObject;
 import org.bson.BasicBSONObject;
+import org.slf4j.Logger;
 import scala.Tuple2;
-import scala.runtime.AbstractFunction0;
 
 import java.util.*;
 
 
 public class Recommender {
 
-    private static String HDFS_HOST = "hdfs://127.0.0.1:9000";
+    private static String HDFS_HOST = "hdfs://crcsmnky.local:9000";
     private static String MONGODB_HOST = "mongodb://127.0.0.1:27017/";
-    private static int SCALE_MAX = 5;
-    private static int SCALE_MIN = 1;
+//    private static int SCALE_MAX = 5;
+//    private static int SCALE_MIN = 1;
 
 //    public static Comparator<Rating> RatingComparator = new Comparator<Rating>() {
 //        @Override
@@ -48,11 +46,9 @@ public class Recommender {
         String moviesUri =  HDFS_HOST + args[2];
         String mongodbUri = MONGODB_HOST + args[3];
 
-        SparkConf conf = new SparkConf()
-            .setAppName("SparkReccommender")
-            .set("spark.default.parallelism", "12");
-
+        SparkConf conf = new SparkConf().setAppName("SparkRecommender");
         JavaSparkContext sc = new JavaSparkContext(conf);
+        Logger log = sc.sc().log();
 
         Configuration bsonDataConfig = new Configuration();
         bsonDataConfig.set("mongo.job.input.format", "com.mongodb.hadoop.BSONFileInputFormat");
@@ -69,14 +65,16 @@ public class Recommender {
                 public Rating call(Tuple2<Object,BSONObject> doc) throws Exception {
                     Integer userid = (Integer) doc._2.get("userid");
                     Integer movieid = (Integer) doc._2.get("movieid");
-                    Double rating = (Double) doc._2.get("rating");
-                    return new Rating(userid, movieid, rating);
+                    Number rating = (Number) doc._2.get("rating");
+                    return new Rating(userid, movieid, rating.doubleValue());
                 }
             }
         );
 
+        log.warn("ratings = " + ratingsData.count());
+
         // keep this RDD in memory as much as possible, and spill to disk if needed
-        ratingsData.persist(StorageLevel.MEMORY_AND_DISK());
+//        ratingsData.persist(StorageLevel.MEMORY_AND_DISK());
 
         JavaRDD<Object> userData = sc.newAPIHadoopFile(usersUri,
             BSONFileInputFormat.class, Object.class, BSONObject.class, bsonDataConfig).map(
@@ -88,6 +86,8 @@ public class Recommender {
             }
         );
 
+        log.warn("users = " + userData.count());
+
         JavaRDD<Object> movieData = sc.newAPIHadoopFile(moviesUri,
             BSONFileInputFormat.class, Object.class, BSONObject.class, bsonDataConfig).map(
             new Function<Tuple2<Object, BSONObject>, Object>() {
@@ -98,8 +98,12 @@ public class Recommender {
             }
         );
 
+        log.warn("movies = " + movieData.count());
+
         // generate complete pairing for all possible (user,movie) combinations
         JavaPairRDD<Object,Object> usersMovies = userData.cartesian(movieData);
+
+        log.warn("usersMovies = " + usersMovies.count());
 
         // create the model from existing ratings data
         MatrixFactorizationModel model = ALS.train(ratingsData.rdd(), 10, 10, 0.01);
@@ -139,7 +143,9 @@ public class Recommender {
             }
         );
 
-        predictionsOutput.repartition(4);
+//        predictionsOutput.repartition(4);
+
+        log.warn("writing " + predictionsOutput.count() + " documents to " + mongodbUri);
 
         predictionsOutput.saveAsNewAPIHadoopFile("file:///notapplicable",
             Object.class, Object.class, MongoOutputFormat.class, predictionsConfig);
